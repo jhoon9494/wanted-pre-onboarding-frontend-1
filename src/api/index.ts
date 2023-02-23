c6c9640acea38dcf35216aa2f74cec5180f98bfa
jhoon9494 <wjdgns9406@gmail.com>
2023-02-23 12:01:59 +0900

    style: axios url 변수 constant 폴더로 이동

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
+import { BACKEND_URL } from '@/constants/apiUrl';
 import axios from 'axios';
 
-const BACKEND_URL = 'https://pre-onboarding-selection-task.shop/';
 const axiosConfig = {
   baseURL: BACKEND_URL,
   headers: {
