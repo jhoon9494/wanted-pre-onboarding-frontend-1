18365e9cdb88c18f9b858ba34161af3a86b3367c
jhoon9494 <wjdgns9406@gmail.com>
2023-02-23 11:24:45 +0900

    feat: 로그인 기능 구현 및 JWT 저장

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,3 +4,7 @@ import IAuth from '@/interface/authType';
 export const signUp = async ({ email, password }: IAuth) => {
   return await API.post('/auth/signup', { email, password });
 };
+
+export const signIn = async ({ email, password }: IAuth) => {
+  return await API.post('/auth/signin', { email, password });
+};
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,19 +1,61 @@
-import { Link } from 'react-router-dom';
+import { AxiosError } from 'axios';
+import { FormEvent, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { validateEmail, validatePassword } from '@/lib/utils/validator';
+import { signIn } from '@/api/auth';
+import { ACCESS_TOKEN } from '@/constants/token';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const res = await signIn({ email, password });
+      localStorage.setItem(ACCESS_TOKEN, res.data.access_token);
+      navigate('/todo', { replace: true });
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        if (e.response?.status === 401) {
+          alert('비밀번호가 다릅니다. 다시 확인해주세요');
+        } else if (e.response?.status === 404) {
+          alert(e.response.data.message);
+        }
+      }
+    }
+  };
+
   return (
     <div>
       <h1>TODO LIST</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           이메일
-          <input type="text" data-testid="email-input" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            data-testid="email-input"
+          />
         </label>
         <label>
           비밀번호
-          <input type="password" data-testid="password-input" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            data-testid="password-input"
+          />
         </label>
-        <button data-testid="signin-button">로그인</button>
+        <button
+          type="submit"
+          data-testid="signin-button"
+          disabled={!validateEmail(email) || !validatePassword(password)}
+        >
+          로그인
+        </button>
       </form>
       <Link to="/signup">아직 회원이 아니신가요?</Link>
     </div>
